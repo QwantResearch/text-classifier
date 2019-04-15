27a7f00c5bf265466510f53309357ec79a6f20d6
NoelM <noel.martin.07@gmail.com>
2019-04-15 17:14:34 +0200

    renaming

--- include/katanoisi/qclassifier.h
+++ include/katanoisi/qclassifier.h
@@ -1,17 +1,16 @@
 // Copyright 2019 Qwant Research. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#ifndef __QCLASSIFIER_H
-#define __QCLASSIFIER_H
+#ifndef __CLASSIFIER_H
+#define __CLASSIFIER_H
 
 #include <fasttext/fasttext.h>
 #include <sstream>
 
 using namespace fasttext;
 
-
-class qclassifier {
+class classifier {
 public:
-  qclassifier(std::string &filename, std::string &domain) :
+  classifier(std::string &filename, std::string &domain) :
     _domain(domain) { _model.loadModel(filename.c_str()); }
 
   std::vector<std::pair<fasttext::real, std::string>> prediction(std::__cxx11::string& text, int count);
@@ -22,5 +21,5 @@ private:
   fasttext::FastText _model;
 };
 
-#endif // __QCLASSIFIER_H
+#endif // __CLASSIFIER_H
 
--- src/classifier.cpp
+++ src/classifier.cpp
@@ -1,9 +1,9 @@
 // Copyright 2019 Qwant Research. Licensed under the terms of the Apache 2.0 license. See LICENSE in the project root.
 
-#include "katanoisi/qclassifier.h"
+#include "katanoisi/classifier.h"
 
 std::vector<std::pair<fasttext::real, std::string>>
-qclassifier::prediction(std::string& text, int count)
+classifier::prediction(std::string& text, int count)
 {
     std::vector<std::pair<fasttext::real, std::string>> results;
 
